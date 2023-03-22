8b32f778f8bf3563b446d1fd9142ac5499c20511
JD Sanchez <67755598+JDSanchz@users.noreply.github.com>
2023-03-22 12:18:52 -0600

    firebase implemented

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import {AiFillEyeInvisible,AiFillEye} from "react-icons/ai"
 import { Link } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { db } from '../firebase';
 
 export default function SignUp() {
     const [showPassword, setShowPassword] = useState(false);
@@ -18,6 +20,17 @@ export default function SignUp() {
             [e.target.id]: e.target.value,
        }))
     }
+    async function onSubmit(e){
+      e.preventDefault()
+      try {
+        const auth = getAuth()
+        const userCredential = await createUserWithEmailAndPassword(auth,email,password)
+        const user = userCredential.user
+        console.log(user)
+      } catch (error) {
+        console.log(error)
+      }
+    }
   return (
     <section>
         <h1 className='text-3xl text-center mt-6 font-bold'>Sign Up</h1>
@@ -26,7 +39,7 @@ export default function SignUp() {
                 <img className='w-full rounded-2xl' src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8a2V5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60" alt="join" />
             </div>
             <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-                <form>
+                <form onSubmit={onSubmit}>
                 <input type="text" id='name' value={name} onChange={onChange} placeholder="Full Name"
                     className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"/>
                     <input type="email" id='email' value={email} onChange={onChange} placeholder="Email Address"
